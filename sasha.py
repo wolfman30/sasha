097e1255c6a7aef91455218bfe035211d9bed9e0
Andrew John Wolf <wolfpassion20@gmail.com>
2018-11-18 11:27:40 -0500

    Added spaces for readability

--- sasha.py
+++ sasha.py
@@ -3,19 +3,21 @@ import speech_recognition as sr
 import os
 import time 
 import webbrowser
+
 def talk(audio):
 	tts=gTTS(text=audio,lang ="en" )
 	tts.save("file1.mp3")
 	os.system("start file1.mp3")
+	
 def listenForCmd():
 
-	
 	r = sr.Recognizer()
 	
 	with sr.Microphone() as source:
 		print("Hello. i am SASHA. i'm online and ready")
 		r.adjust_for_ambient_noise(source, duration = 1)
 		audio = r.listen(source)
+		
 	try:
 		command = r.recognize_google(audio)
 		print(command)
@@ -25,11 +27,15 @@ def listenForCmd():
 		command =listenForCmd()
 		
 	return command
+	
 command = listenForCmd()
+
 def cmd():
 	if "hey" in command:
 		os.system('start sounds/Bidding.mp3')
+
 talk("ready when you are")
+
 while True:
 	listenForCmd()
 	cmd()
